80e300fb7107ec8fce9f57f789f1e7aa12ec7ed9
vichu <vichucoding@gmail.com>
2025-01-07 15:18:23 +0530

    added sign up html page

--- practice/views.py
+++ practice/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from pathlib import Path
+from templates import 'home.html'
 
+this_dir = Path(__file__).resolve().parent
 
 def HomePageView(request, *args, **kwargs):
     return HttpResponse("<h1> Hello world </h1>")
